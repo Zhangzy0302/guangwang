fb369b46b58b98ecb6b4104bbe2bafb46b71194a
zzy <2218847957@qq.com>
2025-05-26 16:05:45 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute top-15 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+        liuxinyu200101012@icloud.com
       </div>
     </div>
   </div>
